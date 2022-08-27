3388b4d9e5e487799cfa765f876bdc202437fd9d
hidankim <hidan.kim0110@gmail.com>
2022-08-28 03:50:25 +0900

    dropdown

--- login.js
+++ login.js
@@ -1,31 +1,3 @@
-const loginbtn = document.getElementById('loginbtn');
-const modal_container = document.getElementById('modal_container');
-const closebtn = document.getElementById('closebtn');
-
-loginbtn.addEventListener('click', () => {
-    modal_container.classList.add('show');
-});
-
-
-closebtn.addEventListener('click', () => {
-    modal_container.classList.remove('show');
-});
-
-const singupbtn = document.getElementById('singupbtn');
-const modal_container2 = document.getElementById('modal_container2');
-const closebtn2 = document.getElementById('closebtn2');
-
-singupbtn.addEventListener('click', () =>{
-    modal_container2.classList.add('show');
-})
-
-closebtn2.addEventListener('click', () => {
-    modal_container2.classList.remove('show');
-})
-
-
-
-
 const selected = document.querySelector(".selected");
 const optionsContainer = document.querySelector(".options-container");
 const optionsList = document.querySelectorAll(".option");
@@ -40,3 +12,6 @@ optionsList.forEach(o => {
     optionsContainer.classList.remove("active");
   });
 });
+
+
+
