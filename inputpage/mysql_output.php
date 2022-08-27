4de21f3026f7f976fefd7e2f5c3616bfdd929ab9
hanmyeonglee <110274577+hanmyeonglee@users.noreply.github.com>
2022-08-28 00:44:09 +0900

    how to get datetime from js

--- inputpage/mysql_output.php
+++ inputpage/mysql_output.php
@@ -4,7 +4,7 @@
    $port = "3306";
    $password = "rBXAm7WN";
    $database = "witheconomy_calendar";
-   $datetime = "2022-08-27";
+   $datetime = $_POST["html variable"]; //방법: index.html에 연결된 javascript에 html의 버튼을 누르면 자동으로 지금 가리키고 있는 날짜를 form tag의 post 형식으로 보내줄 수 있는 코드를 작성해야 함 (input.html과 mysql_input.php를 연결한 방식과 비슷하게)
 
    $con = mysqli_connect($server, $user, $password, $database, $port);
 
