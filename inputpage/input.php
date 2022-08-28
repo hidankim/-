ac57e0c6d6f5ddd21e3b8783533544ec2c3a2a89
hidankim <hidan.kim0110@gmail.com>
2022-08-28 18:06:54 +0900

    input work

--- inputpage/input.php
+++ inputpage/input.php
@@ -4,32 +4,91 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="input.css">
+    <title>Input</title>
 </head>
 <body>
+    <div class="main">
+      
+      <div class="home-button">
+        <a href="login.html">
+          <img src="./pictures/home button 7476c0.png">
+        </a>
+      </div>
 
-    <div>
-        <form action="mysql_input.php" method="post">
-            <div class="container">
-                <h1>Registration</h1>
-                <p>Fill up the form with correct values.</p>
-                <label for="firstname"><b>First Name</b></label>
-                <input type="text" name="firstname" required>
+        <div class="content-wrap">
+          <div class="content-right">
+            <table>
+              <tr>
+                <th>과목</th>
+                <th>날짜</th>
+                <th>교시</th>
+                <th>과목</th>
+                <th>정보</th>
+              </tr>
 
-                <label for="lastname"><b>Last Name</b></label>
-                <input type="text" name="lastname" required>
+            <?php
+            $conn = mysql_connect("locahost", "root", "", "witheconomy_calendar");
+            if ($conn -> connect_error) {
+                die("Connection failed:". $conn=> connect_error);
+            }
+            ?>
 
-                <label for="phonenumber"><b>Email Address</b></label>
-                <input type="email" name="phonenumber" required>
+            </table>
+          </div>
+        </div>
 
-                <label for="firstname"><b>Phone Number</b></label>
-                <input type="text" name="firstname" required>
 
-                <label for="password"><b>Password</b></label>
-                <input type="password" name="password" required>
-            </div>
-        </form>
-    </div>
+        <div class="content-left">
+          <div class="todo-wrap">
+              <form action="mysql_input.php" method="post">
+                <div class="container">
+                      <h1>Add Task</h1>
 
+                      <div>
+                      <label for="subject"><b>과목</b></label>
+                      <input type="text" name="subject" required>
+                      </div>
+
+                      <div>
+                      <label for="date"><b>날짜</b></label>
+                      <input type="date" name="date" required>
+                      </div>
+
+                      <div>
+                      <label for="period"><b>교시</b></label>
+                      <select name="period" required>
+                        <option value="one">1</option>
+                        <option value="two">2</option>
+                        <option value="three">3</option>
+                        <option value="four">4</option>
+                        <option value="five">5</option>
+                        <option value="six">6</option>
+                        <option value="seven">7</option>
+                        <option value="eight">8</option>
+                        <option value="other">외</option>
+                      </select>
+                      </div>
+
+                      <div>
+                      <label for="name"><b>제목</b></label>
+                      <input type="text" name="name" required>
+                      </div>
+
+                      <div>
+                      <label for="info"><b>정보</b></label>
+                      <input type="text" name="info" required>
+                      </div>
+
+                      <div>
+                      <input type="submit" name="create" value="확인">
+                      </div>
+                </div>
+              </form>
+          </div>
+        </div>
+      </div>
+
+      <button type="button" onclick="location.href='mysql_output.php'">값 얻기</button>
 </body>
 </html>
