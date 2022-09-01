1b8211d939dd0bfe3cd39793f5d21cc7cc673167
hanmyeonglee <110274577+hanmyeonglee@users.noreply.github.com>
2022-09-02 08:19:16 +0900

    (대충 ajax 완성했다는 글)

--- inputpage/mysql_output.php
+++ inputpage/mysql_output.php
@@ -17,19 +17,19 @@
 
    $con = mysqli_connect($server, $user, $password, $database, $port);
 
-   $sql = "SELECT title, description, subject, datetime, time FROM schedules WHERE datetime=\"".$datetime."\" && classname=\"".$classname."\""; 
+   $sql = "SELECT title, description, subject, datetime, time FROM schedules WHERE datetime=\"".$datetime."\" && classname=\"".$classname."\"";
 
    $result = mysqli_query($con, $sql);
 
    $row = array();
    while($subrow = mysqli_fetch_row($result))
    {
-      $object = "\"title\":\"{$subrow[0]}\", \"description\":\"{$subrow[1]}\", \"subject\":\"{$subrow[2]}\", \"datetime\":\"{$subrow[3]}\", \"time\":\"{$subrow[4]}\"";
+      $object = '{"title":"'.$subrow[0].'", "description":"'.$subrow[1].'", "subject":"'.$subrow[2].'", "datetime":"'.$subrow[3].'", "time":"'.$subrow[4].'"}';
       array_push($row, $object);
    }
 
    $json = json_encode($row);
-   $bytes = file_put_contents("test_json", $json);
+   $bytes = file_put_contents("convert_json", $json);
 
    mysqli_close($con);
 ?>
