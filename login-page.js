0a5e99c04b7b3a35d943f09664ed76871b606e7c
hanmyeonglee <110274577+hanmyeonglee@users.noreply.github.com>
2022-09-24 20:03:32 +0900

    대충 남은거 수정

--- login-page.js
+++ login-page.js
@@ -8,30 +8,30 @@ loginButton.addEventListener("click", (e) => {
 
     switch(password)
     {
-        case "f3nj22l":window.location.href="./inputpage/input.php?middle_1_1"; break;
-        case "dkm31ef":window.location.href="./inputpage/input.php?middle_1_2"; break;
-        case "ej332dk":window.location.href="./inputpage/input.php?middle_1_3"; break;
-        case "rj32we3":window.location.href="./inputpage/input.php?middle_1_4"; break;
-        case "3kgh2if":window.location.href="./inputpage/input.php?middle_2_1"; break;
-        case "ei3jf0f":window.location.href="./inputpage/input.php?middle_2_2"; break;
-        case "3jskw2t":window.location.href="./inputpage/input.php?middle_2_3"; break;
-        case "dkrj429":window.location.href="./inputpage/input.php?middle_2_4"; break;
-        case "kwris3l":window.location.href="./inputpage/input.php?middle_3_1"; break;
-        case "dm4jflw":window.location.href="./inputpage/input.php?middle_3_2"; break;
-        case "3j5jh3od":window.location.href="./inputpage/input.php?middle_3_3"; break;
-        case "ej4bfk2":window.location.href="./inputpage/input.php?middle_3_4"; break;
-        case "eo2p2ke":window.location.href="./inputpage/input.php?high_1_1"; break;
-        case "3pdo12j":window.location.href="./inputpage/input.php?high_1_2"; break;
-        case "ejfn399":window.location.href="./inputpage/input.php?high_1_3"; break;
-        case "dkwj495":window.location.href="./inputpage/input.php?high_1_4"; break;
-        case "e20dj3h":window.location.href="./inputpage/input.php?high_2_1"; break;
-        case "wj2ndj4":window.location.href="./inputpage/input.php?high_2_2"; break;
-        case "39djth3":window.location.href="./inputpage/input.php?high_2_3"; break;
-        case "2kdjt5i":window.location.href="./inputpage/input.php?high_2_4"; break;
-        case "ei3kw2j":window.location.href="./inputpage/input.php?high_3_1"; break;
-        case "ej40d93":window.location.href="./inputpage/input.php?high_3_2"; break;
-        case "2kejgh4":window.location.href="./inputpage/input.php?high_3_3"; break;
-        case "3l10dpl":window.location.href="./inputpage/input.php?high_3_4"; break;
+        case "f3nj22l":window.location.href="./inputpage/input.html?middle_1_1"; break;
+        case "dkm31ef":window.location.href="./inputpage/input.html?middle_1_2"; break;
+        case "ej332dk":window.location.href="./inputpage/input.html?middle_1_3"; break;
+        case "rj32we3":window.location.href="./inputpage/input.html?middle_1_4"; break;
+        case "3kgh2if":window.location.href="./inputpage/input.html?middle_2_1"; break;
+        case "ei3jf0f":window.location.href="./inputpage/input.html?middle_2_2"; break;
+        case "3jskw2t":window.location.href="./inputpage/input.html?middle_2_3"; break;
+        case "dkrj429":window.location.href="./inputpage/input.html?middle_2_4"; break;
+        case "kwris3l":window.location.href="./inputpage/input.html?middle_3_1"; break;
+        case "dm4jflw":window.location.href="./inputpage/input.html?middle_3_2"; break;
+        case "3j5jh3od":window.location.href="./inputpage/input.html?middle_3_3"; break;
+        case "ej4bfk2":window.location.href="./inputpage/input.html?middle_3_4"; break;
+        case "eo2p2ke":window.location.href="./inputpage/input.html?high_1_1"; break;
+        case "3pdo12j":window.location.href="./inputpage/input.html?high_1_2"; break;
+        case "ejfn399":window.location.href="./inputpage/input.html?high_1_3"; break;
+        case "dkwj495":window.location.href="./inputpage/input.html?high_1_4"; break;
+        case "e20dj3h":window.location.href="./inputpage/input.html?high_2_1"; break;
+        case "wj2ndj4":window.location.href="./inputpage/input.html?high_2_2"; break;
+        case "39djth3":window.location.href="./inputpage/input.html?high_2_3"; break;
+        case "2kdjt5i":window.location.href="./inputpage/input.html?high_2_4"; break;
+        case "ei3kw2j":window.location.href="./inputpage/input.html?high_3_1"; break;
+        case "ej40d93":window.location.href="./inputpage/input.html?high_3_2"; break;
+        case "2kejgh4":window.location.href="./inputpage/input.html?high_3_3"; break;
+        case "3l10dpl":window.location.href="./inputpage/input.html?high_3_4"; break;
         default:alert("Wrong Password"); break;
     }
 })
